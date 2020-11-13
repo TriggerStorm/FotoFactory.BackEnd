2ac4bf660f5700f211d27ee4cd111128e8a24975
TriggerStorm <michael.h.p@hotmail.com>
2020-11-13 15:31:11 +0100

    loginInput and ATHelper

---
+++ FotoFactory.Core/Helper/IAuthenticationHelper.cs
@@ -0,0 +1,15 @@
+﻿using FotoFactory.CoreEntities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace FotoFactory.Core.Helper
+{
+    public interface IAuthenticationHelper
+    {
+        void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
+        bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt);
+        string GenerateToken(User user);
+    }
+}
