ec83813b47242f30c04c49980c1f2d387451e14a
TriggerStorm <michael.h.p@hotmail.com>
2020-11-13 23:07:07 +0100

    start up config
    encruption
    Depentency enj
    Core police

--- FotoFactory.BackEnd/Startup.cs
+++ FotoFactory.BackEnd/Startup.cs
@@ -2,7 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FotoFactory.Core.AppService;
+using FotoFactory.Core.AppService.Service;
+using FotoFactory.Core.DomainService;
+using FotoFactory.Core.Helper;
 using InfraStructure.SQLLite.Data;
+using InfraStructure.SQLLite.Data.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,7 +18,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.IdentityModel.Tokens;
 
 namespace FotoFactory.BackEnd
 {
@@ -21,21 +27,76 @@ namespace FotoFactory.BackEnd
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            //Environment = env;
         }
 
         public IConfiguration Configuration { get; }
+
+        //public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
+            Byte[] secretBytes = new byte[40];
+            Random rand = new Random();
+            rand.NextBytes(secretBytes);
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(secretBytes),
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.FromMinutes(5)
+                };
+            });
+
+
             // if dev do this
             services.AddDbContext<FotoFactoryContext>(
             opt => opt.UseSqlite("Data Source= FotoFactoryApp.db"));
 
+            services.AddScoped<IAuthenticationHelper, AuthenticationHelper>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserService, UserService>();
+
+
+
+            services.AddSingleton<IAuthenticationHelper>(new
+               AuthenticationHelper(secretBytes));
+
+            // Configure the default CORS policy.
+            services.AddCors(options =>
+                options.AddDefaultPolicy(
+                  builder =>
+                  {
+                      builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                  })
+             );
+
+            //Register the Swagger generator using Swashbuckle.
+           /* services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "FotoFactory API",
+                    Description = "A simple example ASP.NET Core Web API"
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });*/
 
 
             services.AddControllers();
+            //services.AddMvc().AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
