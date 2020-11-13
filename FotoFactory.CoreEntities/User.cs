9ac4f50b7eedd4cf8f15991c4c4f20f717124000
SnowBoy73 <56825266+SnowBoy73@users.noreply.github.com>
2020-11-13 13:05:04 +0100

    User entity

---
+++ FotoFactory.CoreEntities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace FotoFactory.CoreEntities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public bool IsAdmin { get; set; }
+        //  public List<WorkSpace> WorkSpaces { get; set; }
+        //  public List<int> Favorites { get; set; }
+
+    }
+}
