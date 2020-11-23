1dece6e295e74229a110b4082c07d3cc2eefdbc2
SnowBoy73 <56825266+SnowBoy73@users.noreply.github.com>
2020-11-23 20:38:42 +0100

    Sizes and Tags in a single Poster

--- InfraStructure.SQLLite.Data/Repositories/PosterRepository.cs
+++ InfraStructure.SQLLite.Data/Repositories/PosterRepository.cs
@@ -19,8 +19,31 @@ namespace InfraStructure.SQLLite.Data.Repositories
 
         public Poster ReadPosterById(int id)
         {
-            return _ctx.Posters.Include(p => p.PosterTags).Include(p => p.PosterSizes).FirstOrDefault(p => p.PosterId == id);
-        }
+            // this works... but no tags
+            // return _ctx.Posters.Include(p => p.PosterTags).Include(p => p.PosterSizes).FirstOrDefault(p => p.PosterId == id);
 
+            // v2 Hacky but works
+            Poster p = _ctx.Posters.Include(p => p.PosterTags).Include(p => p.PosterSizes).FirstOrDefault(p => p.PosterId == id);
+            IEnumerable<PosterTag> ptlist = p.PosterTags;
+            foreach (PosterTag pt in ptlist)
+            {
+                Tag tag = _ctx.Tags.Where(pts => pts.TagId == pt.TagId).FirstOrDefault();
+                pt.Tag.TagId = tag.TagId;
+                pt.Tag.Description = tag.Description;
+                pt.Tag.PosterTags = null;
+            }
+
+            IEnumerable<PosterSize> pslist = p.PosterSizes;
+            foreach (PosterSize ps in pslist)
+            {
+                Size size = _ctx.Sizes.Where(pss => pss.SizeId == ps.SizeId).FirstOrDefault();
+                ps.Size.SizeId = size.SizeId;
+                ps.Size.Dimensions = size.Dimensions;
+                ps.Size.PosterPrice = size.PosterPrice;
+                ps.Size.FramePrice = size.FramePrice;
+                ps.Size.PosterSizes = null;
+            }
+            return p;
+        }
     }
 }
