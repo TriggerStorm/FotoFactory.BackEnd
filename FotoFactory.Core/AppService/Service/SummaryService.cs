9e7a826790b7b58b670d93238b935b84fd8ad9fc
SnowBoy73 <56825266+SnowBoy73@users.noreply.github.com>
2020-12-15 13:19:40 +0100

    Summary classes v1
    returns a list of summaries to front end for display in Sammary tab

--- FotoFactory.Core/AppService/Service/SummaryService.cs
+++ FotoFactory.Core/AppService/Service/SummaryService.cs
@@ -7,34 +7,57 @@ namespace FotoFactory.Core.AppService.Service
 {
     public class SummaryService : ISummaryService
     {
-        public Summary GetSummary(List<WorkSpace> workSpaces)
+        public List<Summary> GetSummaryList(List<WorkSpace> workSpaces)
         {
-            List<WorkSpace>  summaryWorkSpaces = new List<WorkSpace>(); 
-            
-            Summary summary = new Summary();
-
-            summary.TotalPrice = 0;//setting price at zero
+            List<Summary>  allSummaries = new List<Summary>();
+            double totalPrice = 0;//setting cost at zero
+            Summary title = new Summary() {PosterName = "SUMMARY" };
+            allSummaries.Add(title);
+            Summary blank = new Summary() { PosterName = "" };
+            allSummaries.Add(blank);
 
             foreach (WorkSpace workSpace in workSpaces)
             {
                 if (workSpace.WorkSpacePosters.Count != 0)
                 {
+                    Summary workSpaceName = new Summary() { PosterName = workSpace.Name };
+                    allSummaries.Add(workSpaceName);
                     List<WorkSpacePoster> wsp = workSpace.WorkSpacePosters;
                     
-                    summaryWorkSpaces.Add(workSpace);
+                   // summaryWorkSpaces.Add(workSpace);
 
                     foreach (WorkSpacePoster workSpacePoster in wsp)
                     {
-                        summary.TotalPrice += workSpacePoster.Size.PosterPrice;
-                        if (workSpacePoster.Frame.FrameId != 7)
+                        double posterAndFramePrice = 0;
+                        if (workSpacePoster.Frame.FrameType == "NOFRAME")
                         {
-                            summary.TotalPrice += workSpacePoster.Size.FramePrice;
+                            posterAndFramePrice = workSpacePoster.Size.PosterPrice;
                         }
+                        else
+                        {
+                            posterAndFramePrice = workSpacePoster.Size.PosterPrice + workSpacePoster.Size.FramePrice;
+                        }
+                        totalPrice += posterAndFramePrice;
+                        Summary posterDetails = new Summary()
+                        {
+                            PosterName = workSpacePoster.Poster.PosterName,
+                            SKUCode = workSpacePoster.Poster.PosterSku,
+                            Size = workSpacePoster.Size.Dimensions,
+                            Frame = workSpacePoster.Frame.FrameType,
+                            Price = posterAndFramePrice.ToString()
+                        };
+                        allSummaries.Add(posterDetails);
                     }
+                    allSummaries.Add(blank);
                 }
             }
-            summary.SummaryWorkSpaces = summaryWorkSpaces;
-            return summary;
+            Summary totalPriceSummary = new Summary()
+            {
+                Frame = "TOTAL PRICE",
+                Price = totalPrice.ToString()
+            };
+            allSummaries.Add(totalPriceSummary);
+            return allSummaries;
         }
     }
 }
